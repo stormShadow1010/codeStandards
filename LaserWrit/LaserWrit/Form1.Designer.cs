1e0a4ee696762bde29781f7bcb0868a11e69837f
stormShadow1010 <stormShadow1010@tutamail.com>
2024-06-11 22:32:06 +0300

    Add new validation for email addresses

---
+++ LaserWrit/LaserWrit/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LaserWrit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.optionsBox = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(308, 190);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 0;
+            this.goButton.Text = "Create File";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(290, 214);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // optionsBox
+            // 
+            this.optionsBox.FormattingEnabled = true;
+            this.optionsBox.Items.AddRange(new object[] {
+            "Script Ohio",
+            "Scroll",
+            "Other"});
+            this.optionsBox.Location = new System.Drawing.Point(308, 12);
+            this.optionsBox.Name = "optionsBox";
+            this.optionsBox.Size = new System.Drawing.Size(139, 79);
+            this.optionsBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 246);
+            this.Controls.Add(this.optionsBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.goButton);
+            this.Name = "Form1";
+            this.Text = "Laser Writer Data File Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.CheckedListBox optionsBox;
+    }
+}
+
